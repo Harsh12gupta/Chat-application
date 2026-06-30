29849af433c2b9b466c4b3454102753dab0865c6
Harsh12gupta <harsh200456@gmail.com>
2026-06-30 12:32:35 +0530

    added remove admin endpoint

--- chat-service/src/main/java/com/ca/chatservice/controller/ChatController.java
+++ chat-service/src/main/java/com/ca/chatservice/controller/ChatController.java
@@ -3,6 +3,7 @@ package com.ca.chatservice.controller;
 import com.ca.chatservice.dto.AddAdminRequestDTO;
 import com.ca.chatservice.dto.ConversationResponseDTO;
 import com.ca.chatservice.dto.CreateConversationRequestDTO;
+import com.ca.chatservice.dto.RemoveAdminRequestDTO;
 import com.ca.chatservice.service.ChatService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +32,14 @@ public class ChatController {
     public Mono<ResponseEntity<ConversationResponseDTO>> handleAddAdmins(@Valid @RequestBody AddAdminRequestDTO addAdminRequestDTO,
                                                                          @RequestHeader("X-User-Id") UUID adminId, @PathVariable String id){
         return chatService.addAdmins(addAdminRequestDTO,adminId,id).map((c)->{
+            return ResponseEntity.ok().body(c);
+        });
+    }
+
+    @DeleteMapping("/conversation/{id}/admins")
+    public Mono<ResponseEntity<ConversationResponseDTO>> handleRemAdmins(@Valid @RequestBody RemoveAdminRequestDTO removeAdminRequestDTO,
+                                                                         @RequestHeader("X-User-Id") UUID adminId,@PathVariable String id){
+        return chatService.removeAdmins(removeAdminRequestDTO,adminId,id).map(c -> {
             return ResponseEntity.ok().body(c);
         });
     }
